2dc38f2bc7a50719341f0b33b04a3bf2ebfdd728
胡俊 <ainixiaopihai@icloud.com>
2025-12-10 15:33:24 +0800

    fix(router): hide router

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,15 +4,15 @@ import DefaultLayout from '@/component/layout/DefaultLayout.tsx'
 
 const LazyOrder = lazy(() => import('@/pages/order'))
 const LazyPassenger = lazy(() => import('@/pages/passenger'))
-const LazyMine = lazy(() => import('@/pages/mine'))
-const LazyOrderDetail = lazy(() => import('@/pages/orderDetail'))
+// const LazyMine = lazy(() => import('@/pages/mine'))
+// const LazyOrderDetail = lazy(() => import('@/pages/orderDetail'))
 
 const AppRoutes:React.FC = () => (
     <Routes>
         <Route path={'/'} Component={DefaultLayout} children={[
             <Route index={true} element={<LazyOrder />} />,
-            <Route path={'/mine'} element={<LazyMine />} />,
-            <Route path='/mine/orderDetail/:payid' element={<LazyOrderDetail />} />,
+            // <Route path={'/mine'} element={<LazyMine />} />,
+            // <Route path='/mine/orderDetail/:payid' element={<LazyOrderDetail />} />,
             <Route path={'/passenger'} element={<LazyPassenger />} />,
         ]} />
     </Routes>
